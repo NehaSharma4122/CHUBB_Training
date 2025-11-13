0fb294d1f021b6b294b2954982879a780143e3d6
Neha <rmneha1234@gmail.com>
2025-11-13 16:18:01 +0530

    Updated submodule pointer

---
+++ Training_Java_New/src/Books/BooksDetail.java
@@ -0,0 +1,10 @@
+package Books;
+
+public class BooksDetail {
+	public String book_id;
+	public String book_name;
+	public String author_name;
+	public String publisher_name;
+	public String edition;
+	
+}
